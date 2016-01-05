300364f6ff2833a63b7407f715f0aeb9687f937a
jatin-52 <vermajatin240@gmail.com>
2016-01-05 15:50:06 +0530

    form testing

---
+++ form_related_testing.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="http://localhost/angular/angular.js"></script>
+</head>
+<body>
+	<div ng-app="mainApp">
+		<form action="" onsubmit="javascript:void(0)">
+			<input type="text" ng-model="">
+		</form>
+	</div>
+	<script type="text/javascript">
+		angular.module('mainApp', [])
+		.controller('name', function($scope){
+			
+		});
+		
+	</script>
+</body>
+</html>
