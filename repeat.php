f990bd975f6fbfcf11e2833372bc86c1193762a0
jatin-52 <vermajatin240@gmail.com>
2016-01-30 12:42:45 +0530

    angular redefined when i know angular-animate

--- repeat.php
+++ repeat.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Ng repeat</title>
-	<script src="http://localhost/angular/angular.js"></script>
+	<script src="http://localhost/angular/js/angular.js"></script>
 </head>
 <body id="body_id" ng-app="" ng-init='countries = [{"name":"India"},{"name":"Pakistan"},{"name":"German"}]'>
 	<span ng-bind="countries"></span>
@@ -12,5 +12,18 @@
 			{{ 'Country Name = ' + country.name }}
 		</li>
 	</ul>
+
+	<hr>
+
+	<?php 
+		$variable = array( array('this_name' => 'jatin', 'this_name1' => 'jatin1'), 'manish', 'hitesh' );
+		echo "<pre>";
+		print_r($variable);
+		echo "</pre>";
+		echo '<hr>';
+		echo "<pre>";
+		echo json_encode($variable);
+		echo "</pre>";
+	?>
 </body>
 </html>
